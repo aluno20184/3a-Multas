90e4c09f2a3111614c040853f6ce4db86ec17c87
Tiago Correia <aluno20184@ipt.pt>
2019-04-09 14:43:29 +0100

    Alteração da definição das classes do Modelo

---
+++ Multas/Multas/Models/Agentes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Multas.Models
+{
+    public class Agentes
+    {
+        public int ID { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Esquadra { get; set; }
+
+        public string Fotografia { get; set; }
+
+
+
+    }
+}
---
+++ Multas/Multas/Models/Multas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Multas.Models
+{
+    public class Multas
+    {
+
+        public int ID { get; set; }
+
+        public string Infracao { get; set; }
+
+        public string LocalDaMulta { get; set; }
+
+        public decimal ValorMulta { get; set; }
+
+        public DateTime DataDaMulta { get; set; }
+
+
+
+
+    }
+}
